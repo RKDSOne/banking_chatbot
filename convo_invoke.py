1ab7654d4a3eba93ae47986516e38a24dd6a13c7
Gurpreet Singh <gurpreet15140@iiitd.ac.in>
2017-03-19 20:13:27 +0530

    Update convo_invoke.py

--- convo_invoke.py
+++ convo_invoke.py
@@ -8,9 +8,14 @@ conversation = ConversationV1(
 
 workspace_id = 'c9c049e2-9a62-42d8-b8aa-5e80d8fb8406'
 
+user_in = raw_input('Enter Something: ')
+response = conversation.message(workspace_id=workspace_id, message_input={'text': user_in})
+print 'intent: ',response['intents']
+print 'output: ',response['output']['text'],'\n'
+
 while True:
 
 	user_in = raw_input('Enter Something: ')
-	response = conversation.message(workspace_id=workspace_id, message_input={'text': user_in})
+	response = conversation.message(workspace_id=workspace_id, message_input={'text': user_in}, context=response['context'])
 	print 'intent: ',response['intents']
 	print 'output: ',response['output']['text'],'\n'
